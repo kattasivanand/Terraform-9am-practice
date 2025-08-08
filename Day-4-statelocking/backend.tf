9211d527af055b0eb953d11814af7efacce0cafd
katta sivanand <siva@.com>
2025-08-08 12:43:44 +0530

    day-statelock

--- Day-4-statelocking/backend.tf
+++ Day-4-statelocking/backend.tf
@@ -2,6 +2,7 @@ terraform {
   backend "s3" {
     bucket = "day4statelockingmach"
     key = "day-4/terraform.tfstate"
-    region = "us-east-1" 
+    region = "us-east-1"
+    use_lockfile = true #supports latest version >=1.10 below 1.10 it will not support 
   }
 }
